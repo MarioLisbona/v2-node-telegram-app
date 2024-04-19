47e18e2909a740cc0a90604c6fc81d252791555f
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-04-19 15:20:49 +1000

    basic functionality working
    Can send and receive messages on all devides, Desktop, mobile and web browser

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 // Importing necessary modules
 const express = require("express");
 const TelegramBot = require("node-telegram-bot-api");
+const bodyParser = require("body-parser");
 
 // Replace 'YOUR_BOT_TOKEN' with the token you get from BotFather
 const token = "6603956134:AAERpbZH_vmJguvj5pxUZc_QuGcDsWnFLW4";
+// Replace 'YOUR_GROUP_CHAT_ID' with the ID of your group chat
+const chatId = "-1002123153182";
 
 // Create a bot instance
 const bot = new TelegramBot(token, { polling: true });
@@ -12,6 +15,7 @@ messages = [];
 
 bot.on("message", (msg) => {
   const chatId = msg.chat.id;
+  // console.log(chatId);
   messages.push(msg.text);
 
   console.log(
@@ -29,6 +33,9 @@ console.log("Bot is running...");
 // Creating an instance of Express
 const app = express();
 
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+
 // Express route to serve HTML file
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -37,6 +44,22 @@ app.get("/", (req, res) => {
 // route to serve messages array
 app.get("/api/messages", (req, res) => {
   res.json(messages);
+});
+
+// Route to handle messages sent from the client
+app.post("/api/messages", (req, res) => {
+  const msg = req.body.message;
+  messages.push(msg);
+
+  bot
+    .sendMessage(chatId, msg)
+    .then(() => {
+      console.log('"Message Received from Front end-------->"', msg);
+      res.sendStatus(200); // Send a success response
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 });
 
 // Start the server
