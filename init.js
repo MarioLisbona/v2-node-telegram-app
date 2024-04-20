cfb881f8696aa25cbedfb94bc4e7c65587284932
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-04-20 16:02:49 +1000

    move middleware to init.js

--- init.js
+++ init.js
@@ -3,6 +3,9 @@ const express = require("express");
 // Creating an instance of Express
 const app = express();
 
+// Middleware to parse JSON bodies
+app.use(express.json());
+
 messages = [];
 
 module.exports = { app, messages };
