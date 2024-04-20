504bc43ef19866ec00898704aca4e0462089c332
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-04-20 16:03:00 +1000

    Removed unused middleware

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
-// Importing necessary modules
-const bodyParser = require("body-parser");
 require("dotenv").config();
 const { bot } = require("./botSetup");
 const { app } = require("./init");
-const { handleMessage, sendMessageFromClient } = require("./botHandlers");
+const { handleMessage } = require("./botHandlers");
 const indexRoutes = require("./routes/index");
 const apiRoutes = require("./routes/api");
 
-bot.on("message", (msg) => {
-  handleMessage(msg, messages);
-});
+// bot.on("message", (msg) => {
+//   handleMessage(msg, messages);
+// });
 
 console.log("Bot is running...");
-
-// Middleware to parse JSON bodies
-app.use(bodyParser.json());
 
 // Use routes
 app.use("/", indexRoutes);
