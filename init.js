8a632535dc92394c5db94b66ed4fd15177d28ad7
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-04-22 22:03:16 +1000

    comments

--- init.js
+++ init.js
@@ -6,12 +6,15 @@ const app = express();
 // Middleware to parse JSON bodies
 app.use(express.json());
 
+// array of sent messages
 messages = [];
 
+// push new message to messages array
 const setMessages = (msg) => {
   messages.push(msg);
 };
 
+// retrieve the array of messages
 const getMessages = () => messages;
 
 module.exports = { app, messages, setMessages, getMessages };
